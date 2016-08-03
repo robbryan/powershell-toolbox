79cb92f97039ed18c00b1fb7e2c3299d2b422e87
Rob Bryan <robbryan@users.noreply.github.com>
2016-08-03 22:13:53 +0930

    Update Profile.ps1
    Fix to profile

--- Example-Code/Profile.ps1
+++ Example-Code/Profile.ps1
@@ -6,7 +6,7 @@ if ($Host.Name -eq "ConsoleHost")
 {
     $randPart = Get-Random
     $datePart = Get-Date -f yyyy-MM-dd-hhmmss
-    $fileName = "Transcript_{0}_{1}_{2}_{3}.txt" -f $env:COMPUTERNAME, $env:USERNAME, $datePart, $rnd
+    $fileName = "Transcript_{0}_{1}_{2}_{3}.txt" -f $env:COMPUTERNAME, $env:USERNAME, $datePart, $randPart
 
     Start-Transcript -Path (Join-Path "D:\Transcripts" -ChildPath $fileName)
 }
