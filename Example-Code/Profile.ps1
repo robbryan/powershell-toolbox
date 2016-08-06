14e824a71b9538ef201bb9d7d6229f2ed250e6a2
Rob Bryan <robbryan@users.noreply.github.com>
2016-08-06 20:29:37 +0930

    Update Profile.ps1

--- Example-Code/Profile.ps1
+++ Example-Code/Profile.ps1
@@ -4,6 +4,10 @@
 
 if ($Host.Name -eq "ConsoleHost")
 {
+    # Use a colour scheme reminiscent of the Turbo languages...
+    $host.ui.RawUI.ForegroundColor="yellow"
+    $host.ui.RawUI.BackgroundColor="DarkMagenta"
+    
     $randPart = Get-Random
     $datePart = Get-Date -f yyyy-MM-dd-hhmmss
     $fileName = "Transcript_{0}_{1}_{2}_{3}.txt" -f $env:COMPUTERNAME, $env:USERNAME, $datePart, $randPart
