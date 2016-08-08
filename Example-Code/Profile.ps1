a56b7f539b86ed6f5d4bb13109e21dcb4087ce9a
Rob Bryan <robbryan@users.noreply.github.com>
2016-08-08 21:09:00 +0930

    Add History Functions
    Added a few simple functions to Profile.ps1 to import and export
    command-line history.

--- Example-Code/Profile.ps1
+++ Example-Code/Profile.ps1
@@ -2,8 +2,34 @@
 # Profile.ps1 - Record the Windows PowerShell Console session
 #
 
+$env:HISTORYFILE = "~/.powershell_history.csv"
+
+
+function Import-History()
+{
+    If (Test-Path $env:HISTORYFILE)
+    {
+        Import-Csv $env:HISTORYFILE | Add-History
+    }
+}
+
+function Export-History()
+{
+    Get-History | Export-Csv $env:HISTORYFILE
+}
+
+function quit()
+{
+    Export-History
+    exit
+}
+
 if ($Host.Name -eq "ConsoleHost")
 {
+    Import-History
+
+    Register-EngineEvent PowerShell.Exiting -action {quit}
+
     # Use a colour scheme reminiscent of the Turbo languages...
     $host.ui.RawUI.ForegroundColor="yellow"
     $host.ui.RawUI.BackgroundColor="DarkMagenta"
