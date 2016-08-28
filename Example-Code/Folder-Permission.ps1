5cd140b9bc3a8c89632d5f4b4675e6bb09528e99
Rob Bryan <robbryan@users.noreply.github.com>
2016-08-28 20:40:14 +0930

    Add Folder-Permission
    A simple script to add a permission to a folder.

---
+++ Example-Code/Folder-Permission.ps1
@@ -0,0 +1,28 @@
+﻿<#
+    .SYNOPSIS
+        Set the permission specified on a given folder
+
+    .EXAMPLE
+        .\Folder-Permission.ps1 .\Test-Folder "Gotham\Batman" "FullControl"
+#>
+
+[CmdletBinding()]
+Param
+(
+    [Parameter(Mandatory=$true)]
+    $folder,
+    [Parameter(Mandatory=$true)]
+    $domainUser,
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("FullControl", "Modify", "ReadAndExecute")]
+    $permission
+)
+
+$folderPermission = $domainUser,$permission,"ContainerInherit,ObjectInherit","None","Allow"
+
+$folderAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule $folderPermission
+
+$folderAcl = Get-Acl $folder
+$folderAcl.SetAccessRule($folderAccessRule)
+Set-Acl $folder -AclObject $folderAcl
+
