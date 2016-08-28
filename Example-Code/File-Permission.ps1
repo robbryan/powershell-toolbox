0a5a0c49047394fa75ba558e6dae74dd40a32891
Rob Bryan <robbryan@users.noreply.github.com>
2016-08-28 20:52:46 +0930

    Add File-Permission
    A simple script that adds a given permission for a user to a given file.

---
+++ Example-Code/File-Permission.ps1
@@ -0,0 +1,27 @@
+<#
+    .SYNOPSIS
+        Set the permission specified on a given file
+
+    .EXAMPLE
+        .\File-Permission.ps1 .\Test-File.txt "Gotham\Robin" "Modify"
+#>
+
+[CmdletBinding()]
+Param
+(
+    [Parameter(Mandatory=$true)]
+    $file,
+    [Parameter(Mandatory=$true)]
+    $domainUser,
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("FullControl", "Modify", "ReadAndExecute")]
+    $permission
+)
+
+$filePermission = $domainUser,$permission,"Allow"
+
+$fileAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule $filePermission
+
+$fileAcl = Get-Acl $file
+$fileAcl.SetAccessRule($fileAccessRule)
+Set-Acl $file -AclObject $fileAcl
